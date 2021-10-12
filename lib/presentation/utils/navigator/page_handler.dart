cd34fa9674c1fe00193b15c39dd4c80d624d34a2
JuanDAC <juanavila12.0308@gmail.com>
2021-10-12 04:49:04 -0500

    [⚡️] (add preload pageview for best minimilize time of loading that take any pageview UwU)

--- lib/presentation/utils/navigator/page_handler.dart
+++ lib/presentation/utils/navigator/page_handler.dart
@@ -8,26 +8,57 @@ class RouterPageHandler extends ChangeNotifier {
   static RouterPageHandler of(BuildContext context) =>
       Provider.of<RouterPageHandler>(context, listen: false);
 
-  List<Page> get pages => List.unmodifiable(_pages);
+  List<Page> get pages => List.unmodifiable(_pagesPreLoad + _pages);
 
   RouterPageHandler() {
     if (BuddySitterPage.length != BuddySitterLocation.length) {
       throw Error();
     }
-    _pages.add(BuddySitterPage.of(BuddySitterLocation.splahs));
+    show(
+      BuddySitterLocation.splahs,
+      preLoad: BuddySitterLocation.signin,
+      notify: false,
+    );
   }
 
   final List<Page> _pages = [];
+  final List<Page> _pagesPreLoad = [];
 
   BuddySitterPath get currentPath => BuddySitterPath.parse(_pages.last.name);
 
-  void toSignIn() {
-    _pages.add(BuddySitterPage.of(BuddySitterLocation.signin));
-    notifyListeners();
+  void show(
+    String buddySitterLocation, {
+    List<String>? preLoads,
+    String? preLoad,
+    bool notify = true,
+  }) {
+    int index = _pagesPreLoad
+        .indexWhere((element) => BuddySitterLocation.signin == element.name);
+    if (index != -1) {
+      Page<dynamic> page = _pagesPreLoad.removeAt(index);
+      _pages.add(page);
+    } else {
+      _pages.add(BuddySitterPage.of(buddySitterLocation));
+    }
+
+    _pagesPreLoad.clear();
+    if (preLoads != null) {
+      _pagesPreLoad.addAll(
+        preLoads.map(
+          (location) => BuddySitterPage.of(location),
+        ), // preLoad.map
+      ); // _pagesPreLoad.addAll
+    }
+    if (preLoad != null) {
+      _pagesPreLoad.add(BuddySitterPage.of(preLoad));
+    }
+    if (notify) {
+      notifyListeners();
+    }
   }
 
-  void didPop(Page pague) {
-    _pages.remove(pague);
+  void didPop(Page page) {
+    _pages.remove(page);
     notifyListeners();
   }
 
