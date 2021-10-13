7ad66bbe3913261f5b76ac9a1b4ccfb8b82ef25c
JuanDAC <juanavila12.0308@gmail.com>
2021-10-13 14:27:05 -0500

    [➖] (remove dependeci in main)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart' show Focus, runApp;
 import './presentation/app.dart' show BuddySitterApp;
-import './device/web/keyboard.dart' show BuddySitterKeyEventHandler;
 
 void main() {
   runApp(const Focus(
